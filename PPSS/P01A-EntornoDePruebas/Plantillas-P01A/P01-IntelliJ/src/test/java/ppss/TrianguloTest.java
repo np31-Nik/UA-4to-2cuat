62462fbd16632c8d4f0654580a1cae1fc96cadfb
Nikita Polyanskiy <nik.trabajos@gmail.com>
2023-01-31 11:16:05 +0100

    PPSS

---
+++ PPSS/P01A-EntornoDePruebas/Plantillas-P01A/P01-IntelliJ/src/test/java/ppss/TrianguloTest.java
@@ -0,0 +1,58 @@
+package ppss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrianguloTest {
+    int a,b,c;
+    String resultadoReal, resultadoEsperado;
+    Triangulo tri;
+
+    @Test
+    public void testTipo_trianguloC1() {
+       a = 1;
+       b = 1;
+       c = 1;
+       resultadoEsperado = "Equilatero";
+       tri= new Triangulo();
+       resultadoReal = tri.tipo_triangulo(a,b,c);
+       assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void testTipo_trianguloC2() {
+        a = 1;
+        b = 1;
+        c = 11;
+        resultadoEsperado = "No es un triangulo";
+        tri= new Triangulo();
+        resultadoReal = tri.tipo_triangulo(a,b,c);
+        //El método Assert.assertEquals devuelve cierto si el
+        //resultadoEsperado = resultadoReal
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    public void testTipo_trianguloC3() {
+        a = 1;
+        b = 2;
+        c = 0;
+        resultadoEsperado = "Valor c fuera del rango permitido";
+        tri= new Triangulo();
+        resultadoReal = tri.tipo_triangulo(a,b,c);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+
+    @Test
+    public void testTipo_trianguloC4() {
+        a = 14;
+        b = 10;
+        c = 10;
+        resultadoEsperado = "Isosceles";
+        tri= new Triangulo();
+        resultadoReal = tri.tipo_triangulo(a,b,c);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+}
